6dcedd49b80310657f5aa6bb499f4a97dfffafca
Lleyton Magama <157430208+Lleyton20@users.noreply.github.com>
2026-06-09 18:44:54 -0700

    Time: 11 ms (76.29%), Space: 19.2 MB (94.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,20 @@
-from collections import Counter 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # base case anagrams have the same number of letters thus same len 
-        # we could sort the strings and if they equal to each other we can we return true 
-        if len(s) != len(t):
+        count = [0]*26
+        if len(s)!= len(t):
             return False
+        for char in s :
+            count[ord(char)-ord('a')] +=1
 
-        s= Counter(s)
-        t= Counter(t)
-        return s == t
-      
+        for char in t :
+            count[ord(char)-ord('a')] -=1
 
-   
+        for nums in count: # checking if all the characters have the same result 
+            if nums !=0:
+                return False
+            
 
+        return True
         
 
     
